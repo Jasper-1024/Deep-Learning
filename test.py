dcbdf831b58604000cab001142644053f8f10f5d
JasperHale <ljy087621@gmail.com>
2021-08-10 21:59:44 +0800

    7 7.1 7.2
    Signed-off-by: JasperHale <ljy087621@gmail.com>

--- test.py
+++ test.py
@@ -1,11 +1,4 @@
-from keras.datasets import mnist
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+import pydot
 
-print(train_images.ndim,train_images.shape,train_images.dtype)
+print('')
 
-digit = train_images[4]
-
-import matplotlib.pyplot as plt
-
-plt.imshow(digit, cmap=plt.cm.binary)
-plt.show()
